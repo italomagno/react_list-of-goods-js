e2d5bc38b5664d1c46bf53bc27a2490eeedee925
Italo Magno <italomagno10@gmail.com>
2024-11-07 16:49:55 -0300

    https://italomagno.github.io/react_list-of-goods-js/ finishing the challenge

--- src/App.jsx
+++ src/App.jsx
@@ -18,38 +18,37 @@ export const goodsFromServer = [
 export const App = () => {
   const [goodsToSort, setGoodsToSort] = useState(goodsFromServer);
   const [selectedOption, setSelectedOption] = useState("");
+  const [isReverseActive, setIsReverseActive] = useState(false);
 
-  const optionsToSort = [
-    { type: "Sort alphabetically", className: "is-info", isToAppear: true },
-    { type: "Sort by length", className: "is-success", isToAppear: true },
-    { type: "Reverse", className: "is-warning", isToAppear: true },
-    { type: "Reset", className: "is-danger", isToAppear: !checkIfArrayIsEqualsToFromServer() }
-  ];
 
   function handleGoodsAlphabetically() {
-    if(selectedOption === "Reverse") {
-    setGoodsToSort([...goodsToSort].sort((a, b) => a.localeCompare(b)).reverse())
+      setSelectedOption("Sort alphabetically")
+      if(isReverseActive) {
+        setGoodsToSort([...goodsToSort].sort((a, b) => b.localeCompare(a)));
+      } else{
+        setGoodsToSort([...goodsToSort].sort((a, b) => a.localeCompare(b)));
+      }
 
-    } else {
-      setGoodsToSort([...goodsToSort].sort((a, b) => a.localeCompare(b)));
-    }
-
-    setSelectedOption("Sort alphabetically");
   }
 
   function handleSortGoodsByLength() {
+    setSelectedOption('Sort by length')
+    if(isReverseActive) {
+      setGoodsToSort([...goodsToSort].sort((a, b) => b.length - a.length));
+    } else {
     setGoodsToSort([...goodsToSort].sort((a, b) =>  a.length - b.length));
-    setSelectedOption("Sort by length");
+    }
   }
 
   function handleReverseGoods() {
     setGoodsToSort([...goodsToSort].reverse());
-    setSelectedOption("Reverse");
+    setIsReverseActive(!isReverseActive);
   }
 
   function handleGoodsToOriginal() {
     setGoodsToSort(goodsFromServer);
     setSelectedOption("");
+    setIsReverseActive(false);
   }
 
   function checkIfArrayIsEqualsToFromServer() {
@@ -59,37 +58,26 @@ export const App = () => {
   return (
     <div className="section content">
       <div className="buttons">
-        {
-          optionsToSort.map((option) => (
-            option.isToAppear &&
-            <button type="button"
-              onClick={() => {
-                setSelectedOption(option.type);
-                switch (option.type) {
-                  case "Sort alphabetically":
-                    handleGoodsAlphabetically();
-                    break;
-                  case "Sort by length":
-                    handleSortGoodsByLength();
-                    break;
-                  case "Reverse":
-                    handleReverseGoods();
-                    break;
-                  case "Reset":
-                    handleGoodsToOriginal();
-                    break;
-                  default:
-                    break;
-                }
-              }}
-              className={`button ${option.className} ${option.type === selectedOption ? "" : "is-light"}`}
-              key={option.type}
-              data-cy={option.type}
-            >
-              {option.type}
-            </button>
-          ))
-        }
+      <div className="buttons">
+      <button type="button" onClick={handleGoodsAlphabetically} className={`button is-info ${selectedOption !== "Sort alphabetically" ? "is-light": ""}`}>
+        Sort alphabetically
+      </button>
+
+      <button type="button" onClick={handleSortGoodsByLength} className={`button is-success ${selectedOption !== 'Sort by length' ? "is-light": ''}`} >
+        Sort by length
+      </button>
+
+      <button type="button" onClick={handleReverseGoods} className={`button is-warning ${isReverseActive ? '' : "is-light"}`}>
+        Reverse
+      </button>
+
+     { 
+      !checkIfArrayIsEqualsToFromServer() &&
+      <button type="button" onClick={handleGoodsToOriginal} className="button is-danger is-light">
+        Reset
+      </button>
+      }
+    </div>
       </div>
 
       <ul>
